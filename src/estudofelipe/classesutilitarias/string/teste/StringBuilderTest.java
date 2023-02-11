ee6bef9934d3c59f710fe725f79bf07d54f404b6
Felipe Dos Santos <felipe.santosmf2022@gmail.com>
2023-02-11 15:43:09 -0300

    new

--- src/estudofelipe/classesutilitarias/string/teste/StringBuilderTest.java
+++ src/estudofelipe/classesutilitarias/string/teste/StringBuilderTest.java
@@ -2,6 +2,13 @@ package estudofelipe.classesutilitarias.string.teste;
 
 public class StringBuilderTest {
     public static void main(String[] args) {
+        String nome = "Felipe Dos Santos";
 
+        nome.concat(" ADS");//CONCATENAÇÃO
+        System.out.println(nome);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("Felipe").append(" Dos Santos");//CONCATENAÇÃO
+        System.out.println(sb.toString());
     }
 }
